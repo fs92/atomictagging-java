928ccf321196457743e2869079b89a32102ad288
Stephan Mann <stephan@atomictagging.org>
2010-04-09 21:08:44 +0200

    Comment updated

--- org.atomictagging.core/src/org/atomictagging/core/types/CoreTags.java
+++ org.atomictagging.core/src/org/atomictagging/core/types/CoreTags.java
@@ -9,5 +9,9 @@ package org.atomictagging.core.types;
  * @author Stephan Mann
  */
 public final class CoreTags {
+
+	/**
+	 * The tag that gets attached to all atoms pointing to a file on the file system.
+	 */
 	public final static String	FILEREF_TAG	= "x-fileref";
 }

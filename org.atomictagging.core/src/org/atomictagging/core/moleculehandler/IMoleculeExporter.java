6b77bb01c437f1ecd6a6894bd1a2e8fd94a864d7
Stephan Mann <stephan@atomictagging.org>
2010-04-30 07:58:31 +0200

    JavaDoc for molecule handler interfaces

--- org.atomictagging.core/src/org/atomictagging/core/moleculehandler/IMoleculeExporter.java
+++ org.atomictagging.core/src/org/atomictagging/core/moleculehandler/IMoleculeExporter.java
@@ -3,13 +3,34 @@
  */
 package org.atomictagging.core.moleculehandler;
 
+import java.io.File;
+
 import org.atomictagging.core.types.IMolecule;
 
 /**
- * @author Stephan Mann
+ * Interface for classes that can export a molecule to the file system.<br>
+ * <br>
+ * Note: Any implementation that is to be used in an environment must be registered with the
+ * {@link MoleculeHandlerFactory}.
  * 
+ * @author Stephan Mann
  */
 public interface IMoleculeExporter extends IMoleculeHandler {
 
+	/**
+	 * Called by the {@link MoleculeHandlerFactory} to ask whether the given molecule can be handled by this exporter.
+	 * 
+	 * @param molecule
+	 * @return true if the molecule can be handled, false otherwise
+	 */
 	public boolean canHandle( IMolecule molecule );
+
+
+	/**
+	 * Export the given molecule to the given file.
+	 * 
+	 * @param molecule
+	 * @param file
+	 */
+	public void exportToFile( IMolecule molecule, File file );
 }
--- org.atomictagging.core/src/org/atomictagging/core/moleculehandler/IMoleculeViewer.java
+++ org.atomictagging.core/src/org/atomictagging/core/moleculehandler/IMoleculeViewer.java
@@ -6,11 +6,21 @@ package org.atomictagging.core.moleculehandler;
 import org.atomictagging.core.types.IMolecule;
 
 /**
- * @author Stephan Mann
+ * Interface for all classes that are able to view a molecule.<br>
+ * <br>
+ * Note: Any implementation that is to be used in an environment must be registered with the
+ * {@link MoleculeHandlerFactory}.
  * 
+ * @author Stephan Mann
  */
 public interface IMoleculeViewer extends IMoleculeHandler {
 
+	/**
+	 * Called by the {@link MoleculeHandlerFactory} to ask whether the given molecule can be handled by this viewer.
+	 * 
+	 * @param molecule
+	 * @return true if the molecule can be handled, false otherwise
+	 */
 	public boolean canHandle( IMolecule molecule );
 
 }

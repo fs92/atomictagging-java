098b37d23de1ff1151d0f02781860566783986a3
Stephan Mann <stephan@atomictagging.org>
2010-05-19 17:27:15 +0200

    Enhanced cd command to support ".."
    The SetScopeCommand (= cd) now mirrors the file system cd more closely.
    "cd .." now works and subsequent cd can be executed.

--- org.atomictagging.shell/src/org/atomictagging/shell/commands/SetScopeCommand.java
+++ org.atomictagging.shell/src/org/atomictagging/shell/commands/SetScopeCommand.java
@@ -4,8 +4,12 @@
 package org.atomictagging.shell.commands;
 
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
 import org.atomictagging.shell.IShell;
+import org.atomictagging.utils.StringUtils;
 
 /**
  * Command to set the tag scope. Same as "cd" on file systems.
@@ -35,23 +39,47 @@ public class SetScopeCommand extends AbstractCommand {
 
 
 	@Override
-	public int handleInput( String input, PrintStream stdout ) {
-		if ( input.isEmpty() ) {
+	public String getVerboseHelpMessage() {
+		String msg = getHelpMessage();
+		msg += "\n\t\t  Use \"..\" to remove the last item and \"/\" to return to root.";
+		msg += "\n\t\t  Just like \"change directory\" on any file system.";
+		return msg;
+	}
+
+	private final Stack<String>	scope	= new Stack<String>();
+
+
+	@Override
+	public int handleInput( final String input, PrintStream stdout ) {
+
+		if (input.isEmpty()) {
 			return 1;
 		}
 
-		if ( input.equals( "/" ) ) {
+		if (input.equals( "/" )) {
+			scope.clear();
 			shell.setEnvironment( "scope", "" );
 			return 0;
 		}
 
-		if ( input.equals( ".." ) ) {
-			stdout.println( "Sorry, not yet implemented. Try \"cd /\"." );
-			return 1;
+		String in = input;
+		if (input.startsWith( "/" )) {
+			scope.clear();
+			in = input.substring( 1 );
 		}
 
-		shell.setEnvironment( "scope", input );
+		for ( String part : in.split( "/" ) ) {
+			if (part.equals( ".." )) {
+				if (scope.size() > 0) {
+					scope.pop();
+				}
+			} else {
+				scope.push( part );
+			}
+		}
+
+		List<String> result = Arrays.asList( scope.toArray( new String[0] ) );
+		shell.setEnvironment( "scope", StringUtils.join( result, "/" ) );
 		return 0;
 	}
-
 }

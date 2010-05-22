14cea49b4b7ef94c1b77e9c69daaae42296db603
Stephan Mann <stephan@atomictagging.org>
2010-05-22 17:18:26 +0200

    Adjusted welcome message to license.

--- org.atomictagging.shell/src/org/atomictagging/shell/Shell.java
+++ org.atomictagging.shell/src/org/atomictagging/shell/Shell.java
@@ -165,8 +165,10 @@ public class Shell implements IShell {
 
 
 	private static void printWelcomeMessage() {
-		System.out.println( "Welcome to AtomicTagging Shell version 0.0.3" );
-		System.out.println( "Type 'help' to get startet." );
+		System.out.println( "Welcome to Atomic Tagging Shell version 0.0.4. Type 'help' to get startet.\n\n"
+				+ "This program comes with ABSOLUTELY NO WARRANTY. It is free software,\n"
+				+ "and you are welcome to redistribute it under the terms of GPLv3.\n"
+				+ "For more details see COPYING or <http://www.gnu.org/licenses/>." );
 	}
 
 

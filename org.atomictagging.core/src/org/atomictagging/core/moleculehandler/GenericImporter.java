f6c9b864013aeb3c6abb068b7c55cdad6fd99ab9
oros.alexander <oros.alexander@d70030f7-a31d-0969-5bda-b93456cfbb26>
2010-10-19 09:51:12 +0000

    Moved getHashSum from GenericImporter to FileUtils
    git-svn-id: https://atomictagging.googlecode.com/svn/trunk@26 d70030f7-a31d-0969-5bda-b93456cfbb26

--- org.atomictagging.core/src/org/atomictagging/core/moleculehandler/GenericImporter.java
+++ org.atomictagging.core/src/org/atomictagging/core/moleculehandler/GenericImporter.java
@@ -14,9 +14,6 @@
 package org.atomictagging.core.moleculehandler;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -25,11 +22,11 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.atomictagging.core.configuration.Configuration;
 import org.atomictagging.core.services.ATService;
 import org.atomictagging.core.types.Atom;
+import org.atomictagging.core.types.Atom.AtomBuilder;
 import org.atomictagging.core.types.CoreTags;
 import org.atomictagging.core.types.IAtom;
 import org.atomictagging.core.types.IMolecule;
 import org.atomictagging.core.types.Molecule;
-import org.atomictagging.core.types.Atom.AtomBuilder;
 import org.atomictagging.core.types.Molecule.MoleculeBuilder;
 import org.atomictagging.utils.FileUtils;
 import org.atomictagging.utils.StringUtils;
@@ -48,7 +45,7 @@ import org.atomictagging.utils.StringUtils;
 public class GenericImporter implements IMoleculeImporter {
 
 	@Override
-	public boolean canHandle( File file ) {
+	public boolean canHandle( final File file ) {
 		return true;
 	}
 
@@ -86,30 +83,30 @@ public class GenericImporter implements IMoleculeImporter {
 			isRemote = false;
 		}
 
-		String fileName = copyFile( file, targetDirName );
+		final String fileName = copyFile( file, targetDirName );
 		if ( fileName == null ) {
 			System.out.println( "Error. No file imported." );
 			return;
 		}
 
-		IAtom filename = Atom.build().withData( file.getName() ).withTag( "filename" ).buildWithDataAndTag();
-		AtomBuilder binRefBuilder = Atom.build().withData( "/" + fileName ).withTag( CoreTags.FILEREF_TAG ).withTag(
-				CoreTags.FILETYPE_UNKNOWN );
+		final IAtom filename = Atom.build().withData( file.getName() ).withTag( "filename" ).buildWithDataAndTag();
+		final AtomBuilder binRefBuilder = Atom.build().withData( "/" + fileName ).withTag( CoreTags.FILEREF_TAG )
+				.withTag( CoreTags.FILETYPE_UNKNOWN );
 
 		if ( isRemote ) {
 			binRefBuilder.withTag( CoreTags.FILEREF_REMOTE_TAG );
 		}
 
-		MoleculeBuilder mBuilder = Molecule.build().withAtom( filename ).withAtom( binRefBuilder.buildWithDataAndTag() )
-				.withTag( "generic-file" );
+		final MoleculeBuilder mBuilder = Molecule.build().withAtom( filename )
+				.withAtom( binRefBuilder.buildWithDataAndTag() ).withTag( "generic-file" );
 
 		if ( isRemote ) {
-			IAtom remote = Atom.build().withData( repository ).withTag( CoreTags.FILEREF_REMOTE_LOCATION )
+			final IAtom remote = Atom.build().withData( repository ).withTag( CoreTags.FILEREF_REMOTE_LOCATION )
 					.buildWithDataAndTag();
 			mBuilder.withAtom( remote );
 		}
 
-		IMolecule molecule = mBuilder.buildWithAtomsAndTags();
+		final IMolecule molecule = mBuilder.buildWithAtomsAndTags();
 		ATService.getMoleculeService().save( molecule );
 		molecules.add( molecule );
 	}
@@ -120,26 +117,26 @@ public class GenericImporter implements IMoleculeImporter {
 	 * @param targetDirName
 	 * @return
 	 */
-	public static String saveFile( final byte[] bytes, String targetDirName ) {
+	public static String saveFile( final byte[] bytes, final String targetDirName ) {
 		System.out.println( "Saving file..." );
 
-		String hash = DigestUtils.md5Hex( bytes );
-		List<String> pathArray = new ArrayList<String>( 3 );
+		final String hash = DigestUtils.md5Hex( bytes );
+		final List<String> pathArray = new ArrayList<String>( 3 );
 		pathArray.add( hash.substring( 0, 2 ) );
 		pathArray.add( hash.substring( 2, 4 ) );
 
-		File targetDir = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
+		final File targetDir = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
 
 		pathArray.add( hash.substring( 4 ) );
 
-		File target = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
+		final File target = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
 		try {
 			targetDir.mkdirs();
 			target.createNewFile();
 
 			FileUtils.saveFile( bytes, target );
 
-		} catch ( Exception e ) {
+		} catch ( final Exception e ) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
@@ -155,23 +152,23 @@ public class GenericImporter implements IMoleculeImporter {
 	 * @param targetDirName
 	 * @return The resulting file name the file was copied to without the repository directory
 	 */
-	public static String copyFile( final File file, String targetDirName ) {
+	public static String copyFile( final File file, final String targetDirName ) {
 		System.out.println( "Copying file..." );
-		String hash = getHashSum( file );
-		List<String> pathArray = new ArrayList<String>( 3 );
+		final String hash = FileUtils.getHashSum( file );
+		final List<String> pathArray = new ArrayList<String>( 3 );
 		pathArray.add( hash.substring( 0, 2 ) );
 		pathArray.add( hash.substring( 2, 4 ) );
 
-		File targetDir = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
+		final File targetDir = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
 
 		pathArray.add( hash.substring( 4 ) );
 
-		File target = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
+		final File target = new File( targetDirName + "/" + StringUtils.join( pathArray, "/" ) );
 		try {
 			targetDir.mkdirs();
 			target.createNewFile();
 			FileUtils.copyFile( file, target );
-		} catch ( Exception e ) {
+		} catch ( final Exception e ) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
@@ -179,23 +176,6 @@ public class GenericImporter implements IMoleculeImporter {
 
 		System.out.println( "Created file: " + target.getAbsolutePath() );
 		return StringUtils.join( pathArray, "/" );
-	}
-
-
-	private static String getHashSum( final File file ) {
-		String hash = null;
-		try {
-			FileInputStream fis = new FileInputStream( file );
-			hash = DigestUtils.md5Hex( fis );
-			fis.close();
-		} catch ( FileNotFoundException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch ( IOException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return hash;
 	}
 
 }

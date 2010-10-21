a7fc7037ef180625d2d3ca8c62816ab026f12525
oros.alexander <oros.alexander@d70030f7-a31d-0969-5bda-b93456cfbb26>
2010-10-21 13:47:15 +0000

    new part ImageBrowserPart
    git-svn-id: https://atomictagging.googlecode.com/svn/trunk@47 d70030f7-a31d-0969-5bda-b93456cfbb26

---
+++ org.atomictagging.ui/src/org/atomictagging/ui/parts/ImageBrowserPart.java
@@ -0,0 +1,26 @@
+package org.atomictagging.ui.parts;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author strangeoptics
+ * 
+ */
+public class ImageBrowserPart {
+
+	/**
+	 * @param parent
+	 */
+	@Inject
+	public ImageBrowserPart( final Composite parent ) {
+	}
+
+
+	@Focus
+	public void onFocus() {
+	}
+
+}
